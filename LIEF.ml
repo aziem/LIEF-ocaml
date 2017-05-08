9bb1a7e98245380ab9997ecb81e009e97fa94fc2
Aziem Chawdhary <aziem@chawdhary.co.uk>
2017-05-08 22:32:33 +0100

    Better structure in OCaml API

--- LIEF.ml
+++ LIEF.ml
@@ -8,29 +8,38 @@ module F = Ffi_generated
 module ELF =
 struct
 
+  module ELFBinary =
+  struct
+
+    let elf_binary_get_name elf_binary =
+      getf (!@ elf_binary) (B.E.ElfBinary.name)
+        
+    let elf_get_sections elf_binary =
+      let open B.E in
+      let sections = getf (!@ elf_binary) (ElfBinary.sections) in
+      let rec loop acc p =
+        match FT.coerce (ptr ElfSection.elf_section_t) (FT.ptr_opt ElfSection.elf_section_t) !@p with
+        | None -> List.rev acc
+        | Some s -> loop (s :: acc) (p +@ 1)
+      in
+      loop [] sections
+        
+    let elf_get_num_of_sections elf_binary =
+      List.length (elf_get_sections elf_binary)
+
+  end
+  
+  module ELFSection =
+  struct
+    
+    let get_name elf_section =
+      getf (!@ elf_section) (B.E.ElfSection.name)
+        
+  end
+
   let elf_parse = B.elf_parse_
 
   let elf_destroy = B.elf_binary_destroy_
 
-  let elf_get_name elf_binary =
-    getf (!@ elf_binary) (B.E.ElfBinary.name)
-
-  let elf_get_sections elf_binary =
-    let open B.E in
-    let sections = getf (!@ elf_binary) (ElfBinary.sections) in
-    let rec loop acc p =
-      match FT.coerce (ptr ElfSection.elf_section_t) (FT.ptr_opt ElfSection.elf_section_t) !@p with
-      | None -> List.rev acc
-      | Some s -> loop (s :: acc) (p +@ 1)
-    in
-    loop [] sections
-
-  module ELFSection =
-  struct
-
-    let get_name elf_section =
-      getf (!@ elf_section) (B.E.ElfSection.name)
-
-  end
 end
          
--- test.ml
+++ test.ml
@@ -2,7 +2,7 @@ open LIEF
 
 let _ =
   let elf_file = ELF.elf_parse "/bin/ls" in
-  Printf.printf "ELF Binary name: %s\n" (ELF.elf_get_name elf_file);
-  let sec = ELF.elf_get_sections elf_file in
+  Printf.printf "ELF Binary name: %s\n" (ELF.ELFBinary.elf_binary_get_name elf_file);
+  let sec = ELF.ELFBinary.elf_get_sections elf_file in
   List.iter (fun s -> Printf.printf "Section name: %s\n" (ELF.ELFSection.get_name s)) sec;
   ELF.elf_destroy elf_file 
