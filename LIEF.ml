61a913499cd3f4b1122f59a1c91f374c46495b78
Aziem Chawdhary <aziem@chawdhary.co.uk>
2017-05-08 16:51:31 +0100

    Add get name function

--- LIEF.ml
+++ LIEF.ml
@@ -4,9 +4,14 @@ open Foreign
 module B = Ffi_bindings.Bindings(Ffi_generated_types)(Ffi_generated)
 
 module ELF =
-  struct
-    let elf_parse = B.elf_parse_
+struct
 
-    let elf_destroy = B.elf_binary_destroy_
-  end
+  let elf_parse = B.elf_parse_
+
+  let elf_destroy = B.elf_binary_destroy_
+
+  let elf_get_name elf_binary =
+    getf (!@ elf_binary) (B.E.ElfBinary.name)
+
+end
          
--- test.ml
+++ test.ml
@@ -2,5 +2,5 @@ open LIEF
 
 let _ =
   let elf_file = ELF.elf_parse "/bin/ls" in
-  let () = ELF.elf_destroy elf_file in
-  ()
+  Printf.printf "ELF Binary name: %s\n" (ELF.elf_get_name elf_file);
+  ELF.elf_destroy elf_file 
