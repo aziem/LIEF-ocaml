8469150880df7e31fb190c1b938dafdd34507390
Aziem Chawdhary <aziem@chawdhary.co.uk>
2017-05-17 16:59:29 +0100

    Update test to print out symbols

--- test.ml
+++ test.ml
@@ -5,4 +5,10 @@ let _ =
   Printf.printf "ELF Binary name: %s\n" (ELF.ELFBinary.elf_binary_get_name elf_file);
   let sec = ELF.ELFBinary.elf_get_sections elf_file in
   List.iter (fun s -> Printf.printf "Section name: %s\n" (ELF.ELFSection.get_name s)) sec;
+  let dynsym = ELF.ELFBinary.elf_get_dynamic_symbols elf_file in
+  List.iter (fun s -> Printf.printf "Dynamic Symbol: %s\n" (ELF.ELFSymbol.get_name s)) dynsym;
+
+  let staticsym = ELF.ELFBinary.elf_get_static_symbols elf_file in
+  List.iter (fun s -> Printf.printf "Static Symbol: %s\n" (ELF.ELFSymbol.get_name s)) staticsym;
+  
   ELF.elf_destroy elf_file 
